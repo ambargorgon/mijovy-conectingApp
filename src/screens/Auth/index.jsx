fe2e17de93575d47e61ed866bb9caf6ec001be75
Ambar Gorgon <ambargorgon@gmail.com>
2023-07-02 19:13:03 -0300

    updates

--- src/screens/Auth/index.jsx
+++ src/screens/Auth/index.jsx
@@ -10,12 +10,9 @@ import styles from "./style";
 import Card from "../../components/Card";
 import { useDispatch } from "react-redux";
 import { signUp } from "../../store/actions/auth.action";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import Input from "../../components/input";
 import 'expo-dev-client'
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import auth from "@react-native-firebase/auth"
 
 const SignIn = ({ newStyles, navigation }) => {
   const dispatch = useDispatch();
@@ -24,10 +21,6 @@ const SignIn = ({ newStyles, navigation }) => {
   const [error, setError] = useState(null);
   
   const FORM_INPUT_UPDATE = "FORM_INPUT_UPDATE";
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber
-  }, [])
 
   if (user){
     console.log(user)
@@ -38,42 +31,6 @@ const SignIn = ({ newStyles, navigation }) => {
   }else{
     console.log("no user")
   }
-
-  //Sign in con Google 
-  GoogleSignin.configure({
-    webClientId: '467375038847-o452bdtfrhhjofgkssrpd1d82hr6s461.apps.googleusercontent.com',
-  })
-
-  const onGoogleButtonPress = async () => {
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-    const { idToken } = await GoogleSignin.signIn();
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-    const user_sign_in = auth().signInWithCredential(googleCredential)
-    user_sign_in.then((user) => {
-      console.log("userGoogleObjeto", user)
-      auth().onAuthStateChanged(auth, (user) => {
-        if (user) {
-          setUser(user);
-          if (user) {
-            AsyncStorage.setItem(
-              "UserLoggedInData",
-              JSON.stringify({ user, loggedIn: true })
-            )
-          }
-        }
-      });
-    }).catch((error) =>
-      console.log(error))
-  }
-
-  function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) setInitializing(false)
-
-    console.log(user)
-  }
-
-  if(initializing)return null
 
   const formReducer = (state, action) => {
     console.log(action);
@@ -99,7 +56,6 @@ const SignIn = ({ newStyles, navigation }) => {
     return state;
   };
 
-
   const [formState, dispatchFormState] = useReducer(formReducer, {
     inputValues: {
       email: "",
@@ -113,7 +69,6 @@ const SignIn = ({ newStyles, navigation }) => {
     },
     formIsValid: false,
   });
-
 
   const onInputChangeHandler = useCallback(
     (inputIdentifier, inputValue, inputValidity) => {
@@ -210,15 +165,6 @@ const SignIn = ({ newStyles, navigation }) => {
           <Text>Ya eres usuario?</Text>
           <Text style={styles.SignIn}>Inicia Sesion</Text>
         </TouchableOpacity>
-        <View style={styles.mediaContainer}>
-          <Card otherStyles={styles.mediaCard}>
-            <TouchableOpacity  styles={styles.mediaCard} onPress={() => onGoogleButtonPress().then(() =>
-              console.log("Sign in with Google"))}>
-              <Ionicons name="logo-google" size={25} />
-              <Text style={styles.signInText}>Ingresar con Google</Text>
-            </TouchableOpacity>
-          </Card>
-        </View>
       </View>
     </View>
   );
